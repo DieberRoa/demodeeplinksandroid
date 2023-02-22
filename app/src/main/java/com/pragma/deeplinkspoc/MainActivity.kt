647519f96531dfb791cf34abddd84d48a9346b8b
AugustoRoa <augusto.roa@pragma.com.co>
2023-02-22 15:39:48 -0500

    Se añade configuración

--- app/src/main/java/com/pragma/deeplinkspoc/MainActivity.kt
+++ app/src/main/java/com/pragma/deeplinkspoc/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pragma.deeplinkspoc.navigation.AppNavigation
 import com.pragma.deeplinkspoc.ui.theme.DeeplinkspocTheme
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +35,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        // ATTENTION: This was auto-generated to handle app links.
     }
 }
 
